d1540d5b786e6111179e7c1905a0d2b3d9b5d91d
shaid30044 <kaona30044@gmail.com>
2024-02-07 18:22:13 +0600

    Sphere

---
+++ 1011.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double R, pi = 3.14159, volume;
+
+    scanf("%lf", &R);
+
+    volume = (4.0 / 3.0) * pi * R * R * R;
+
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
