22a88b61e511692faa9b5d1caa0732412763f065
shaid30044 <kaona30044@gmail.com>
2024-02-16 20:34:09 +0600

    Sequence IJ 4

---
+++ 1098.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    float i, j;
+
+    for (i = 0; i <= 2.2; i += 0.2)
+    {
+        for (j = 1; j <= 3; j++)
+        {
+            if ((i > 0 && i < 1) || (i > 1 && i < 2))
+            {
+                printf("I=%.1f J=%.1f\n", i, j + i);
+            }
+            else
+            {
+                printf("I=%.0f J=%.0f\n", i, j + i);
+            }
+        }
+    }
+
+    return 0;
+}
