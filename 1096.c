cc6973c2b7ad07c8af59e3ff0a82e519079e6c5b
shaid30044 <kaona30044@gmail.com>
2024-02-16 18:35:33 +0600

    Sequence IJ 2

--- 1096.c
+++ 1096.c
@@ -2,15 +2,14 @@
 
 int main()
 {
-    int i, j, jIdx = 7;
+    int i, j;
 
     for (i = 1; i <= 9; i += 2)
     {
-        for (j = jIdx; j >= jIdx - 2; j--)
+        for (j = 7; j >= 5; j--)
         {
             printf("I=%d J=%d\n", i, j);
         }
-        jIdx += 2;
     }
 
     return 0;
