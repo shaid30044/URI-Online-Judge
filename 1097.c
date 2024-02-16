1310380c40e59e042c7a2cf2642c1f93388fba60
shaid30044 <kaona30044@gmail.com>
2024-02-16 20:33:15 +0600

    Sequence IJ 3

--- 1097.c
+++ 1097.c
@@ -2,21 +2,15 @@
 
 int main()
 {
-    float i, j;
+    int i, j, jIdx = 7;
 
-    for (i = 0; i <= 2.2; i += 0.2)
+    for (i = 1; i <= 9; i += 2)
     {
-        for (j = 1; j <= 3; j++)
+        for (j = jIdx; j >= jIdx - 2; j--)
         {
-            if ((i > 0 && i < 1) || (i > 1 && i < 2))
-            {
-                printf("I=%.1f J=%.1f\n", i, j + i);
-            }
-            else
-            {
-                printf("I=%.0f J=%.0f\n", i, j + i);
-            }
+            printf("I=%d J=%d\n", i, j);
         }
+        jIdx += 2;
     }
 
     return 0;
